abbfd02ab4352c5aa3b418aad3d58094021c08f5
Kevin12J <143035696+Kevin12J@users.noreply.github.com>
2025-04-02 15:46:14 -0700

    Started file add Lambda

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-
+# This repository contains AWS Lambda functions for updating storage usage information
+## Resources
+- [AWS Lambda S3](https://docs.aws.amazon.com/lambda/latest/dg/with-s3-example.html)
+- [S3 Event Notifications](https://docs.aws.amazon.com/AmazonS3/latest/userguide/EventNotifications.html)
---
+++ addFile/app.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import json
+import urllib.parse
+import boto3
+from botocore.exceptions import ClientError
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    try:
+        storage_usage_key = key.split("/")[0]+"/usage.json"
+        # Get storage usage file
+        try:
+            response = s3.get_object(Bucket=bucket, Key=storage_usage_key)
+            storage_data = json.loads(response['Body'].read().decode('utf-8'))
+        except ClientError as e:
+            # Handle the key not existing and initializing default values
+            if e.response['Error']['Code'] == 'NoSuchKey':
+                print(f"{storage_usage_key} does not exist. Creating a new one.")
+                storage_data = {"fileCount": 0, "totalBytes": 0}
+            # Other errors
+            else:
+                raise e
+
+        # Get the previous storage usage metrics
+        file_count = storage_data.get('fileCount', 0)
+        total_bytes = storage_data.get('totalBytes', 0)
+ 
+        # Get the size of the added file
+        new_file_size = event['Records'][0]['s3']['object']['size']
+        file_count += 1
+        total_bytes += new_file_size
+
+        # Update the storage data JSON
+        storage_data['fileCount'] = file_count
+        storage_data['totalBytes'] = total_bytes
+        s3.put_object(
+            Bucket=bucket,
+            Key=storage_usage_key,  
+            Body=json.dumps(storage_data),
+            ContentType='application/json'
+        )
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'fileCount': file_count,
+                'totalBytes': total_bytes,
+                'message': 'Storage data updated successfully'
+            })
+        }
+
+    except Exception as e:
+        print(f"Error processing S3 add: {e}")
+        raise e
+
+
+

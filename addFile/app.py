457756195f1a58e3b96c81bbcdbbf2a94a192fc7
Taanish R <tnreja@gmail.com>
2025-04-05 01:49:16 -0700

    Got stats updates working for file upload and deletion
    Co-authored-by: Kevin Jacob <Kevin12J@users.noreply.github.com>

--- addFile/app.py
+++ addFile/app.py
@@ -1,58 +1,49 @@
 #!/usr/bin/env python3
 import json
 import os
-from dotenv import load_dotenv
 import urllib.parse
-import boto3
-from botocore.exceptions import ClientError
+import psycopg2
 
-load_dotenv()
-s3 = boto3.client('s3')
+CONN_STRING = os.getenv("CONN_STRING")
+db = psycopg2.connect(CONN_STRING)
 
-USAGE_BUCKET = os.getenv("USAGE_BUCKET")
 
 def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     try:
-        storage_usage_key = key.split("/")[0]+"/usage.json"
-        # Get storage usage file
-        try:
-            response = s3.get_object(Bucket=USAGE_BUCKET, Key=storage_usage_key)
-            storage_data = json.loads(response['Body'].read().decode('utf-8'))
-        except ClientError as e:
-            # Handle the key not existing and initializing default values
-            if e.response['Error']['Code'] == 'NoSuchKey':
-                print(f"{storage_usage_key} does not exist. Creating a new one.")
-                storage_data = {"fileCount": 0, "totalBytes": 0}
-            # Other errors
-            else:
-                raise e
-
-        # Get the previous storage usage metrics
-        file_count = storage_data.get('fileCount', 0)
-        total_bytes = storage_data.get('totalBytes', 0)
+        org_id = key.split("/")[0]
+        file_id = f"file_{key.split("/")[2].split('_')[1]}"
+        cursor = db.cursor()
+        query = """
+                INSERT INTO resource_usage (organization_id, file_count, total_bytes)
+                VALUES (%s, %s, %s)
+                ON CONFLICT (organization_id)
+                DO UPDATE SET
+                    file_count = resource_usage.file_count + %s,
+                    total_bytes = resource_usage.total_bytes + %s;
+                """
  
         # Get the size of the added file
         new_file_size = event['Records'][0]['s3']['object']['size']
-        file_count += 1
-        total_bytes += new_file_size
 
-        # Update the storage data JSON
-        storage_data['fileCount'] = file_count
-        storage_data['totalBytes'] = total_bytes
-        s3.put_object(
-            Bucket=USAGE_BUCKET,
-            Key=storage_usage_key,  
-            Body=json.dumps(storage_data),
-            ContentType='application/json'
-        )
+        data = (org_id,1,new_file_size,1,new_file_size)
 
-        return {
+        # Update the storage data 
+        cursor.execute(query,data)
+
+        file_size_query = """
+            UPDATE files SET file_size = %s WHERE file_id = %s
+        """
+        
+        cursor.execute(file_size_query, (new_file_size,file_id))
+
+        db.commit()
+        cursor.close()
+
+        return {        
             'statusCode': 200,
             'body': json.dumps({
-                'fileCount': file_count,
-                'totalBytes': total_bytes,
                 'message': 'Storage data updated successfully'
             })
         }
--- deleteFile/app.py
+++ deleteFile/app.py
@@ -1,64 +1,44 @@
 #!/usr/bin/env python3
 import json
 import os
-from dotenv import load_dotenv
 import urllib.parse
-import boto3
-from botocore.exceptions import ClientError
+import psycopg2
 
-load_dotenv()
-s3 = boto3.client('s3')
-
-USAGE_BUCKET = os.getenv("USAGE_BUCKET")
+CONN_STRING = os.getenv("CONN_STRING")
+db = psycopg2.connect(CONN_STRING)
 
 def lambda_handler(event, context):
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    key = event["key"]
+    removed_file_size = event["totalBytes"]
+    
     try:
-        storage_usage_key = key.split("/")[0]+"/usage.json"
-        # Get storage usage file
-        try:
-            response = s3.get_object(Bucket=USAGE_BUCKET, Key=storage_usage_key)
-            storage_data = json.loads(response['Body'].read().decode('utf-8'))
-        except ClientError as e:
-            # Handle the key not existing and initializing default values
-            if e.response['Error']['Code'] == 'NoSuchKey':
-                print(f"{storage_usage_key} does not exist. Creating a new one.")
-                storage_data = {"fileCount": 0, "totalBytes": 0}
-            # Other errors
-            else:
-                raise e
+        org = key.split("/")[0]
+        cursor = db.cursor()
+        query = """
+                INSERT INTO resource_usage (organization_id, file_count, total_bytes)
+                VALUES (%s, %s, %s)
+                ON CONFLICT (organization_id)
+                DO UPDATE SET
+                    file_count = resource_usage.file_count - %s,
+                    total_bytes = resource_usage.total_bytes - %s;
+                """
 
-        # Get the previous storage usage metrics
-        file_count = storage_data.get('fileCount', 0)
-        total_bytes = storage_data.get('totalBytes', 0)
- 
-        # Get the size of the removed file
-        remove_file_size = event['Records'][0]['s3']['object']['size']
-        file_count = max(0,file_count-1)
-        total_bytes = max(0,total_bytes-remove_file_size)
+        data = (org,1,removed_file_size,1,removed_file_size)
 
-        # Update the storage data JSON
-        storage_data['fileCount'] = file_count
-        storage_data['totalBytes'] = total_bytes
-        s3.put_object(
-            Bucket=USAGE_BUCKET,
-            Key=storage_usage_key,  
-            Body=json.dumps(storage_data),
-            ContentType='application/json'
-        )
+        # Update the storage data 
+        cursor.execute(query,data)
+        db.commit()
+        cursor.close()
 
-        return {
+        return {        
             'statusCode': 200,
             'body': json.dumps({
-                'fileCount': file_count,
-                'totalBytes': total_bytes,
                 'message': 'Storage data updated successfully'
             })
         }
 
     except Exception as e:
-        print(f"Error processing S3 add: {e}")
+        print(f"Error processing S3 remove: {e}")
         raise e
 
 
